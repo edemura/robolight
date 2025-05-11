52a6bb42b7496cedea19a671a88ec026a2cbc6e4
edemura <demura.egor@gmail.com>
2025-05-11 20:09:49 +0300

    added Tube_qty class --fix

--- users/models.py
+++ users/models.py
@@ -494,7 +494,7 @@ class Orders(models.Model):
         verbose_name="Вид последовательности"
     )
 
-    tube_qty = models.ForeignKey(
+    tube_type = models.ForeignKey(
       "users.TubeType",
       on_delete=models.CASCADE,
       verbose_name="Тип пробирки"
