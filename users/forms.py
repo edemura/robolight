69456960606d30e08ba4024edc5c1f3491ac81f2
edemura <demura.egor@gmail.com>
2025-05-13 21:28:12 +0300

    added fix forms

--- users/forms.py
+++ users/forms.py
@@ -15,7 +15,7 @@ ch = [[1, 'ЦКДЛ'], [2, 'Внутренняя лаборатория']]
 class OrdersForm(forms.Form):
     #tube_type=TubeType.make_choices()[0]
     
-    tube_type1=forms.IntegerField(max_value=10, min_value=0, label="TubeType.make_choices()[0]")
+    #tube_type1=forms.IntegerField(max_value=10, min_value=0, label="TubeType.make_choices()[0]")
     #tube_type2=forms.IntegerField(max_value=10, min_value=0, label=TubeType.make_choices()[1])  
     #tube_type3=forms.IntegerField(max_value=10, min_value=0, label=TubeType.make_choices()[2])  
     
