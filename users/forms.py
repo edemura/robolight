cbcca31092d83852f911d28ee6e6acf19448acbc
edemura <demura.egor@gmail.com>
2025-05-31 19:16:11 +0300

    added --modified css by cursor edit form

--- users/forms.py
+++ users/forms.py
@@ -19,4 +19,10 @@ class OrdersForm(forms.Form):
     #tube_type2=forms.IntegerField(max_value=10, min_value=0, label=TubeType.make_choices()[1])  
     #tube_type3=forms.IntegerField(max_value=10, min_value=0, label=TubeType.make_choices()[2])  
     
-    destination = forms.ChoiceField(choices=Sequences.make_choices(), label='Для направления в ')
+    destination = forms.ChoiceField(
+        choices=Sequences.make_choices(),
+        label='Для направления в',
+        widget=forms.Select(attrs={
+            'class': 'form-select',
+        })
+    )
